28f19df6eaa19f2c46b481b3ae3d2a1ca8471f2e
peter <petergitzz@icloud.com>
2020-03-28 23:35:26 +0300

    added jobs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blogs(models.Model):
+    title =models.CharField(max_length=200)
+    pub_date=models.DateField(auto_now_add=False)
+    body = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="images/")
