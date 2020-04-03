98d95c8a34b0adb6adad27d3819a2ab16c6a2c85
peter <petergitzz@icloud.com>
2020-04-03 23:58:50 +0000

    tweeks

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 try:
-    from . local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
