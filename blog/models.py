563d4dd0ecc8c7dc7c287f234d9478f007874c5a
peter <petergitzz@icloud.com>
2020-04-03 04:05:07 +0300

    final

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Blogs(models.Model):
-    title =models.CharField(max_length=200)
+    title =models.CharField(max_length=500)
     pub_date=models.DateField(auto_now_add=False)
     body = models.CharField(max_length=200)
     image = models.ImageField(upload_to="images/")
+
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
